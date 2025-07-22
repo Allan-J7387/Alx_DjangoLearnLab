1ca7ed0f000e46eb0d1732f7ad239b4d7409f93b
Allan.J <langatjayallan@gmail.com>
2025-07-22 06:57:05 +0300

    eigth commit

--- django-models/LibraryProject/bookshelf/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Book
-
-def list_books(request):
-    books = Book.objects.all()  # ✅ Required: Use Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # ✅ Required path
