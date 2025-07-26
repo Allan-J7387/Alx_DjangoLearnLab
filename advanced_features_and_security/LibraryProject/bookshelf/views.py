39428ebff63eaa4ecb0797aa9c8d042e0c7165a4
Allan.J <langatjayallan@gmail.com>
2025-07-26 22:29:01 +0300

    views.py updated

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,3 +4,15 @@ from .models import Book
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
+
+
+
+
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render
+from .models import Book  
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
