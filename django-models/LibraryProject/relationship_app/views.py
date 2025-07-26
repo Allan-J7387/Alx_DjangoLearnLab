c231da3c36d0fcb669ff1d6bda89f5cb1d49f3b4
Allan.J <langatjayallan@gmail.com>
2025-07-26 19:46:36 +0300

    views.py updated

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -39,6 +39,19 @@ def delete_book(request, book_id):
 from django.shortcuts import render
 from .models import Book
 
+
+from django.shortcuts import render
+from .models import Book
+
 def list_books(request):
     books = Book.objects.all()
-    return render(request, "relationship_app/list_books.html", {"books": books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+
+from django.views.generic.detail import DetailView
+from .models import Library  # ✅ Required import
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # ✅ Required template
+    context_object_name = 'library'  # ✅ Object name for use in template
