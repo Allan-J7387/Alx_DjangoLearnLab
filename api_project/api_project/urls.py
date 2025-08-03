eeb54e95a2a1a7144914842140b00952f56c0a14
Allan.J <langatjayallan@gmail.com>
2025-08-03 11:41:06 +0300

    api endpoints updated

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -20,3 +20,12 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+from django.contrib import admin
+from django.urls import path, include  # ✅ include is needed
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # ✅ include API URLs
+]
