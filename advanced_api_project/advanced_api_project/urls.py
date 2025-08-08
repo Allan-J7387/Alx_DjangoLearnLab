1dff3937e16f1a5d6506201cf71aa359471cc43f
Allan.J <langatjayallan@gmail.com>
2025-08-08 12:41:33 +0300

    fitteenth commit

--- advanced_api_project/advanced_api_project/urls.py
+++ advanced_api_project/advanced_api_project/urls.py
@@ -19,3 +19,14 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+
+from django.contrib import admin
+from django.urls import path
+from api.views import hello_api
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/hello/', hello_api),
+]
