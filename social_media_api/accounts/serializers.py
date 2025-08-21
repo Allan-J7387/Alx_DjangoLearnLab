8c7d6dd4b91ad4976ca4dbf03c515e32c09955ae
Allan.J <langatjayallan@gmail.com>
2025-08-21 12:39:39 +0300

    serializers.py updated

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
+serializers.CharField()
+
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
